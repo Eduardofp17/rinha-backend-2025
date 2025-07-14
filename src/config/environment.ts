709b9a66d7b6cffef4b8c737702edf96fa1d0cbe
Eduardofp <edardofernandespinheiro@gmail.com>
2025-07-14 13:21:08 -0300

    fix: removing DB_URL from environment, I forgot to remove it

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,5 +3,4 @@ export const ENVIRONMENT = {
   PAYMENT_PROCESSOR_URL_FALLBACK: process.env.PROCESSOR_FALLBACK_URL ?? 'http://payment-processor-fallback:8080',
   APP_PORT: process.env.APP_PORT ?? 8080,
   REDIS_URL: process.env.REDIS_URL,
-  DB_URL: process.env.DB_URL, 
 }
