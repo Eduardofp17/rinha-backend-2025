ba54b5d43efad8a1379e5ab92b3a1a5ef08c63bc
Eduardofp <edardofernandespinheiro@gmail.com>
2025-08-03 09:44:09 -0300

    refactor: update payment-handler to reflect new processing flow

--- src/handlers/payment-handler.ts
+++ src/handlers/payment-handler.ts
@@ -1,15 +1,21 @@
 import type {FastifyRequest, FastifyReply} from 'fastify';
 import type { PaymentRequest } from '../domain/payment';
 import {validate} from 'uuid';
-import {queue} from '../infra/queue';
+import {queue, redis} from '../infra/queue';
 
 export const paymentHandler = async (req: FastifyRequest<PaymentRequest>, rep: FastifyReply) => {
   const {correlationId, amount} = req.body;
-  
   if(!correlationId || !amount || !validate(correlationId) || amount <= 0) return rep.status(400).send();
+   const exists = await redis.hexists('payments-correlationIds', correlationId);
+   if (exists) { 
+    return rep.status(202).send();
+   }
+
   const priority = amount > 1000 ? 1 : amount > 100 ? 2 : 3;
 
-  await queue.add("payment",{correlationId, amount: Number(amount)}, {
+  await redis.hset('payments-correlationIds', correlationId, 'processing');
+
+  queue.add("payment",{correlationId, amount: Math.round(amount * 100)}, {
         priority,
     delay: 0,
   });
